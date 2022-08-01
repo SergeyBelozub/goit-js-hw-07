f364932f9b54b18590355c123b5546f572bf9917
SergeyBelozub <99657458+SergeyBelozub@users.noreply.github.com>
2022-08-01 11:19:11 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onModalMarkup);
+
+function onModalMarkup(e) {
+  e.preventDefault();
+
+  let instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onCloseEsc);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onCloseEsc);
+      },
+    }
+  );
+  function onCloseEsc(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    } else {
+      return;
+    }
+  }
+  instance.show();
+  return instance;
+}
